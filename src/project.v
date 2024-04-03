a4dfb486a6c2807005a049917aca2af0905b047f
Lucaz97 <lc4976@nyu.edu>
2024-04-03 14:01:54 -0400

    added excursions

--- src/project.v
+++ src/project.v
@@ -55,11 +55,22 @@ module tt_um_lucaz97_monobit (
       .valid_triosy_lz(),
       .epsilon_rsc_dat(ui_in[0]),
       .epsilon_triosy_lz());
+
+      excursions excursions_inst (
+      .clk(clk),
+      .rst_n(rst_n),
+      .is_random_rsc_dat(uio_out[0]),
+      .is_random_triosy_lz(),
+      .valid_rsc_dat(uio_out[1]),
+      .valid_triosy_lz(),
+      .epsilon_rsc_dat(ui_in[0]),
+      .epsilon_triosy_lz()
+    );
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out[7:6] = 0;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
+  assign uio_out[7:2] = 0;
+  assign uio_oe[7:2]  = 0;
+  assign uio_oe[1:0]  = 2'b11;
 endmodule
 
 
@@ -163,6 +174,246 @@ module mgc_io_sync_v2 (ld, lz);
     assign lz = ld;
 
 endmodule
+
+
+//------> /eda/mentor/Siemens_EDA/Catapult_Synthesis_2023.1_2/Mgc_home/pkgs/siflibs/mgc_shift_bl_beh_v5.v 
+module mgc_shift_bl_v5(a,s,z);
+   parameter    width_a = 4;
+   parameter    signd_a = 1;
+   parameter    width_s = 2;
+   parameter    width_z = 8;
+
+   input [width_a-1:0] a;
+   input [width_s-1:0] s;
+   output [width_z -1:0] z;
+
+   generate if ( signd_a )
+   begin: SGNED
+     assign z = fshl_s(a,s,a[width_a-1]);
+   end
+   else
+   begin: UNSGNED
+     assign z = fshl_s(a,s,1'b0);
+   end
+   endgenerate
+
+   //Shift-left - unsigned shift argument one bit more
+   function [width_z-1:0] fshl_u_1;
+      input [width_a  :0] arg1;
+      input [width_s-1:0] arg2;
+      input sbit;
+      parameter olen = width_z;
+      parameter ilen = width_a+1;
+      parameter len = (ilen >= olen) ? ilen : olen;
+      reg [len-1:0] result;
+      reg [len-1:0] result_t;
+      begin
+        result_t = {(len){sbit}};
+        result_t[ilen-1:0] = arg1;
+        result = result_t <<< arg2;
+        fshl_u_1 =  result[olen-1:0];
+      end
+   endfunction // fshl_u
+
+   //Shift-left - unsigned shift argument
+   function [width_z-1:0] fshl_u;
+      input [width_a-1:0] arg1;
+      input [width_s-1:0] arg2;
+      input sbit;
+      fshl_u = fshl_u_1({sbit,arg1} ,arg2, sbit);
+   endfunction // fshl_u
+
+   //Shift right - unsigned shift argument
+   function [width_z-1:0] fshr_u;
+      input [width_a-1:0] arg1;
+      input [width_s-1:0] arg2;
+      input sbit;
+      parameter olen = width_z;
+      parameter ilen = signd_a ? width_a : width_a+1;
+      parameter len = (ilen >= olen) ? ilen : olen;
+      reg signed [len-1:0] result;
+      reg signed [len-1:0] result_t;
+      begin
+        result_t = $signed( {(len){sbit}} );
+        result_t[width_a-1:0] = arg1;
+        result = result_t >>> arg2;
+        fshr_u =  result[olen-1:0];
+      end
+   endfunction // fshl_u
+
+   //Shift left - signed shift argument
+   function [width_z-1:0] fshl_s;
+      input [width_a-1:0] arg1;
+      input [width_s-1:0] arg2;
+      input sbit;
+      reg [width_a:0] sbit_arg1;
+      begin
+        // Ignoring the possibility that arg2[width_s-1] could be X
+        // because of customer complaints regarding X'es in simulation results
+        if ( arg2[width_s-1] == 1'b0 )
+        begin
+          sbit_arg1[width_a:0] = {(width_a+1){1'b0}};
+          fshl_s = fshl_u(arg1, arg2, sbit);
+        end
+        else
+        begin
+          sbit_arg1[width_a] = sbit;
+          sbit_arg1[width_a-1:0] = arg1;
+          fshl_s = fshr_u(sbit_arg1[width_a:1], ~arg2, sbit);
+        end
+      end
+   endfunction
+
+endmodule
+
+//------> /eda/mentor/Siemens_EDA/Catapult_Synthesis_2023.1_2/Mgc_home/pkgs/siflibs/mgc_shift_l_beh_v5.v 
+module mgc_shift_l_v5(a,s,z);
+   parameter    width_a = 4;
+   parameter    signd_a = 1;
+   parameter    width_s = 2;
+   parameter    width_z = 8;
+
+   input [width_a-1:0] a;
+   input [width_s-1:0] s;
+   output [width_z -1:0] z;
+
+   generate
+   if (signd_a)
+   begin: SGNED
+      assign z = fshl_u(a,s,a[width_a-1]);
+   end
+   else
+   begin: UNSGNED
+      assign z = fshl_u(a,s,1'b0);
+   end
+   endgenerate
+
+   //Shift-left - unsigned shift argument one bit more
+   function [width_z-1:0] fshl_u_1;
+      input [width_a  :0] arg1;
+      input [width_s-1:0] arg2;
+      input sbit;
+      parameter olen = width_z;
+      parameter ilen = width_a+1;
+      parameter len = (ilen >= olen) ? ilen : olen;
+      reg [len-1:0] result;
+      reg [len-1:0] result_t;
+      begin
+        result_t = {(len){sbit}};
+        result_t[ilen-1:0] = arg1;
+        result = result_t <<< arg2;
+        fshl_u_1 =  result[olen-1:0];
+      end
+   endfunction // fshl_u
+
+   //Shift-left - unsigned shift argument
+   function [width_z-1:0] fshl_u;
+      input [width_a-1:0] arg1;
+      input [width_s-1:0] arg2;
+      input sbit;
+      fshl_u = fshl_u_1({sbit,arg1} ,arg2, sbit);
+   endfunction // fshl_u
+
+endmodule
+
+//------> ../td_ccore_solutions/leading_sign_20_0_cece25dae2d00b43c3b124ab661316c967d1_0/rtl.v 
+// ----------------------------------------------------------------------
+//  HLS HDL:        Verilog Netlister
+//  HLS Version:    2023.1_2/1049935 Production Release
+//  HLS Date:       Sat Jun 10 10:53:51 PDT 2023
+// 
+//  Generated by:   lc4976@hansolo.poly.edu
+//  Generated date: Fri Mar 29 18:01:13 2024
+// ----------------------------------------------------------------------
+
+// 
+// ------------------------------------------------------------------
+//  Design Unit:    leading_sign_20_0
+// ------------------------------------------------------------------
+
+
+module leading_sign_20_0 (
+  mantissa, rtn
+);
+  input [19:0] mantissa;
+  output [4:0] rtn;
+
+
+  // Interconnect Declarations
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_6_2_sdt_2;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_18_3_sdt_3;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_26_2_sdt_2;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_42_4_sdt_4;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_6_2_sdt_1;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_14_2_sdt_1;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_26_2_sdt_1;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_34_2_sdt_1;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_50_2_sdt_1;
+  wire c_h_1_2;
+  wire c_h_1_5;
+  wire c_h_1_6;
+  wire c_h_1_8;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_75_ssc;
+
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_nl;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_or_1_nl;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_2_nl;
+  wire ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leadi000000;
+
+  // Interconnect Declarations for Component Instantiations 
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_6_2_sdt_2
+      = ~((mantissa[17:16]!=2'b00));
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_6_2_sdt_1
+      = ~((mantissa[19:18]!=2'b00));
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_14_2_sdt_1
+      = ~((mantissa[15:14]!=2'b00));
+  assign c_h_1_2 = ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_6_2_sdt_1
+      & ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_6_2_sdt_2;
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_18_3_sdt_3
+      = (mantissa[13:12]==2'b00) & ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_14_2_sdt_1;
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_26_2_sdt_2
+      = ~((mantissa[9:8]!=2'b00));
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_26_2_sdt_1
+      = ~((mantissa[11:10]!=2'b00));
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_34_2_sdt_1
+      = ~((mantissa[7:6]!=2'b00));
+  assign c_h_1_5 = ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_26_2_sdt_1
+      & ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_26_2_sdt_2;
+  assign c_h_1_6 = c_h_1_2 & ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_18_3_sdt_3;
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_42_4_sdt_4
+      = (mantissa[5:4]==2'b00) & ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_34_2_sdt_1
+      & c_h_1_5;
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_50_2_sdt_1
+      = ~((mantissa[3:2]!=2'b00));
+  assign c_h_1_8 = c_h_1_6 & ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_42_4_sdt_4;
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_75_ssc
+      = (mantissa[1:0]==2'b00) & ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_50_2_sdt_1
+      & c_h_1_8;
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_nl
+      = c_h_1_6 & (~ ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_42_4_sdt_4);
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_or_1_nl
+      = (c_h_1_2 & (c_h_1_5 | (~ ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_18_3_sdt_3))
+      & (~ c_h_1_8)) | ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_75_ssc;
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_2_nl
+      = ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_6_2_sdt_1
+      & (ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_14_2_sdt_1
+      | (~ ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_6_2_sdt_2))
+      & (~((~(ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_26_2_sdt_1
+      & (ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_34_2_sdt_1
+      | (~ ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_26_2_sdt_2))))
+      & c_h_1_6)) & (ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_wrs_c_50_2_sdt_1
+      | (~ c_h_1_8)) & (~ ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_75_ssc);
+  assign ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leadi000000
+      = ~((mantissa[19]) | (~((mantissa[18:17]!=2'b01))) | (((mantissa[15]) | (~((mantissa[14:13]!=2'b01))))
+      & c_h_1_2) | ((~((~((mantissa[11]) | (~((mantissa[10:9]!=2'b01))))) & (~(((mantissa[7])
+      | (~((mantissa[6:5]!=2'b01)))) & c_h_1_5)))) & c_h_1_6) | (((mantissa[3]) |
+      (~((mantissa[2:1]!=2'b01)))) & c_h_1_8) | ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_75_ssc);
+  assign rtn = {c_h_1_8 , ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_nl
+      , ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_or_1_nl
+      , ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_and_2_nl
+      , ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leading_sign_20_0_rtn_ac_math_ac_normalize_20_20_false_AC_TRN_AC_WRAP_leading_1_leadi000000};
+endmodule
+
 
 
 //------> ./rtl.v 
